6e8c6ad94b8d6a1bfcdebf26e9c58a6292035c73
Locobuzz-Prakhranshu-Singh <168509171+Locobuzz-Prakhranshu-Singh@users.noreply.github.com>
2024-07-30 03:50:43 +0530

    Prakhar [Add]: Adding the code of QR code generator

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using IronBarCode;
+using IronSoftware.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QR
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            string link = "https://in.linkedin.com/";
+            string file = "C:\\Users\\Admin\\source\\repos\\QR codes\\demo12.png";
+            string logofile = "C:\\Users\\Admin\\source\\repos\\QR codes\\locobuzz_logo.jpg";
+            int errorCorrectionLevel = (int)QRCodeWriter.QrErrorCorrectionLevel.Highest;
+            QRCodeLogo logo = new QRCodeLogo(logofile);
+            //GeneratedBarcode barcode = IronBarCode.BarcodeWriter.CreateBarcode(link,BarcodeEncoding.QRCode);
+            //barcode.SaveAsPng("C:\\Users\\Admin\\source\\repos\\QR codes\\"+file);
+            GeneratedBarcode QRcodewithLogo = QRCodeWriter.CreateQrCodeWithLogo(link, logo, errorCorrectionLevel);
+            QRcodewithLogo.ResizeTo(500, 500).SetMargins(15).ChangeBarCodeColor(Color.Black);
+            QRcodewithLogo.SaveAsPng(file);
+
+        }
+    }
+}
